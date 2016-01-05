100a0ccb57df19a0e55db969243fe17902f6fc3a
Greg Anders <ganders@credera.com>
2016-01-05 09:26:53 -0600

    Add mongolab URI

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://mongo/slackbots');
+mongoose.connect(process.env.MONGOLAB_URI);
 
 var Bot = require('./models/Bot');
 
